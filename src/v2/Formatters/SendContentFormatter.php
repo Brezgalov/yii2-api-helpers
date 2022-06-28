261d2607686a4dda806113d3cd2632c9afbbda88
Oleg Brezgalov <brezgalov.developer@gmail.com>
2022-06-28 15:25:57 +0300

    senders formatters

---
+++ src/v2/Formatters/SendContentFormatter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Brezgalov\ApiHelpers\v2\Formatters;
+
+use Brezgalov\ApiHelpers\v2\Formatters\ModelResultFormatter;
+use Brezgalov\ApiHelpers\v2\IFormatter;
+use yii\web\Response;
+
+class SendContentFormatter extends ModelResultFormatter implements IFormatter
+{
+    /**
+     * @var Response
+     */
+    public $response;
+
+    /**
+     * @var string|callable
+     */
+    public $attachmentName;
+
+    /**
+     * @var array
+     */
+    public $sendOptions = [];
+
+    /**
+     * SendFileFormatter constructor.
+     * @param array $config
+     */
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+
+        if (empty($this->response) && \Yii::$app->has('response')) {
+            $this->response = \Yii::$app->response;
+        }
+    }
+
+    /**
+     * @param object $service
+     * @param mixed $result
+     * @return \yii\base\Model|Response
+     */
+    public function format($service, $result)
+    {
+        if ($result === false) {
+            parent::format($service, $result);
+        }
+
+        $response = clone $this->response;
+
+        $fileName = null;
+        if ($this->attachmentName) {
+            $fileName = is_callable($this->attachmentName) ? (
+            call_user_func($this->attachmentName, $service, $result)
+            ) : $this->attachmentName;
+        }
+
+        return $response->sendContentAsFile($result, $fileName, $this->sendOptions);
+    }
+}
---
+++ src/v2/Formatters/SendFileFormatter.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Brezgalov\ApiHelpers\v2\Formatters;
+
+use Brezgalov\ApiHelpers\v2\Formatters\ModelResultFormatter;
+use Brezgalov\ApiHelpers\v2\IFormatter;
+use yii\base\InvalidArgumentException;
+use yii\web\Response;
+
+class SendFileFormatter extends ModelResultFormatter implements IFormatter
+{
+    /**
+     * @var Response
+     */
+    public $response;
+
+    /**
+     * @var bool
+     */
+    public $dropOnSend = false;
+
+    /**
+     * @var string|callable
+     */
+    public $attachmentName;
+
+    /**
+     * @var array
+     */
+    public $sendOptions = [];
+
+    /**
+     * SendFileFormatter constructor.
+     * @param array $config
+     */
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+
+        if (empty($this->response) && \Yii::$app->has('response')) {
+            $this->response = \Yii::$app->response;
+        }
+    }
+
+    /**
+     * @param object $service
+     * @param mixed $result
+     * @return \yii\base\Model|Response
+     */
+    public function format($service, $result)
+    {
+        if ($result === false) {
+            parent::format($service, $result);
+        }
+
+        if (!is_string($result)) {
+            throw new InvalidArgumentException('Service $result value supposed to be string');
+        }
+
+        if (!is_file($result)) {
+            throw new InvalidArgumentException("Can not find file \"{$result}\"");
+        }
+
+        $response = clone $this->response;
+        if ($this->dropOnSend) {
+            $response->on(Response::EVENT_AFTER_SEND, function ($event) {
+                if (is_file($event->data['unlinkFile'])) {
+                    unlink($event->data['unlinkFile']);
+                }
+            }, ['unlinkFile' => $result]);
+        }
+
+        $fileName = null;
+        if ($this->attachmentName) {
+            $fileName = is_callable($this->attachmentName) ? (
+                call_user_func($this->attachmentName, $service, $result)
+            ) : $this->attachmentName;
+        }
+
+        return $response->sendFile($result, $fileName, $this->sendOptions);
+    }
+}
